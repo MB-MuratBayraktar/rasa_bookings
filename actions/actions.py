b14d4334471cebb939b13261722e25e207d9d9ac
murat <murat.bayraktar@prodato.de>
2023-03-05 21:18:43 +0100

    added functioning forms with basic sots NO DUCKLING YET

--- actions/actions.py
+++ actions/actions.py
@@ -6,23 +6,41 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+from typing import Text, List, Any, Dict
 
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Tracker, FormValidationAction, Action
+from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import AllSlotsReset
-#
-#
-#
-class ActionHelloWorld(Action):
+from rasa_sdk.types import DomainDict
+
+ALLOWED_CITIES = ["berlin", "munich", "frankfurt", "hamburg"]
+ALLOWED_ROOM_TYPES =["single", "double", "suit"]
+
+class ValidateSimpleBookingForm(FormValidationAction):
 
     def name(self) -> Text:
-        return "action_undo_reservation"
+        return "validate_simple_booking_form"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def validate_room_type(
+        self,slot_value: Any,dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `room_type` value."""
 
-        dispatcher.utter_message(text="Alright then, lets start over")
+        if slot_value.lower() not in ALLOWED_ROOM_TYPES:
+            dispatcher.utter_message(text=f"Currently, you can only book one of the following room types: single/double/suit")
+            return {"room_type": None}
+        dispatcher.utter_message(text=f"OK! You want to book a {slot_value} room.")
+        return {"room_type": slot_value}
+        
+    def validate_city(self, slot_value: Any, dispatcher: CollectingDispatcher,tracker: Tracker,
+                       domain: DomainDict) -> Dict[Text, Any]:
+        """validate the city"""
 
-        return [AllSlotsReset()]
+        if slot_value.lower() not in ALLOWED_CITIES:
+            dispatcher.utter_message(text=f"the current available rooms are only located in those cities: {'/'.join(ALLOWED_CITIES)}, \n Please choose one of them.") 
+            return {"city":None}
+        dispatcher.utter_message(text=f"OK! your reservation will be set in {slot_value}")
+        return {"city":slot_value}
+    
