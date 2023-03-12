ec1136a2765783da8cd147f110811193eafbc6de
murat <murat.bayraktar@prodato.de>
2023-03-12 10:50:00 +0100

    added full name personalization to the form

--- actions/actions.py
+++ actions/actions.py
@@ -15,7 +15,13 @@ from rasa_sdk.types import DomainDict
 import dateutil.parser
 
 
+def clean_name(name):
+    return "".join([c for c in name if c.isalpha()])
+
 class ValidateBookingForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_booking_form"
 
     @staticmethod
     def allowed_cities() -> List[Text] :
@@ -25,9 +31,36 @@ class ValidateBookingForm(FormValidationAction):
     def allowed_rooms() -> List[Text]:
         return ["single", "double", "suit"]
     
-    def name(self) -> Text:
-        return "validate_booking_form"
+    
+    def validate_first_name(self,slot_value: Any, dispatcher: CollectingDispatcher,
+                      tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
+        
+        name = clean_name(slot_value)
 
+        if len(slot_value) == 0:
+            dispatcher.utter_message(text=f"that's too short for a first name, I think you mis-spelled your name.")
+            return {"first_name": None}
+        else:
+            dispatcher.utter_message(text=f"pleased to meet you {slot_value}")
+            return {"first_name": name}
+    
+    def validate_last_name(self,slot_value : Any, dispatcher: CollectingDispatcher,
+                      tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
+        
+        last_name = clean_name(slot_value)
+
+        if len(slot_value) ==0:
+            dispatcher.utter_message(text=f"I think you mis-spelled your surname.")
+            return {"last_name": None}
+        else:
+            reg_first_name = tracker.get_slot("first_name")
+            if len(reg_first_name) + len(last_name) < 3: 
+                dispatcher.utter_message(text="this is too short for a full name, i suspect a type. Restarting!")
+                return {"first_name":None,"last_name":None}
+            
+            dispatcher.utter_message(text=f"Ok, successfuly registered your full name as {reg_first_name} {slot_value}.")    
+            return {"last_name": slot_value}
+    
     def validate_room_type(
         self,slot_value: Any,dispatcher: CollectingDispatcher,
         tracker: Tracker,
@@ -38,8 +71,9 @@ class ValidateBookingForm(FormValidationAction):
         if slot_value.lower() not in self.allowed_rooms():
             dispatcher.utter_message(text=f"Currently, you can only book one of the following room types: {'/'.join(self.allowed_rooms())}")
             return {"room_type": None}
-        dispatcher.utter_message(text=f"OK! You want to book a {slot_value} room.")
-        return {"room_type": slot_value}
+        else:
+            dispatcher.utter_message(text=f"OK! You want to book a {slot_value} room.")
+            return {"room_type": slot_value}
         
     def validate_city(self, slot_value: Any, dispatcher: CollectingDispatcher,tracker: Tracker,
                        domain: DomainDict) -> Dict[Text, Any]:
@@ -48,8 +82,9 @@ class ValidateBookingForm(FormValidationAction):
         if slot_value.lower() not in self.allowed_cities():
             dispatcher.utter_message(text=f"the current available rooms are only located in those cities: {'/'.join(self.allowed_cities())}, \n Please choose one of them.") 
             return {"city":None}
-        dispatcher.utter_message(text=f"OK! your reservation will be set in {slot_value}")
-        return {"city":slot_value}
+        else:
+            dispatcher.utter_message(text=f"OK! your reservation will be set in {slot_value}")
+            return {"city":slot_value}
     
     def validate_time(self, slot_value: Any, dispatcher: CollectingDispatcher,tracker: Tracker,
                        domain: DomainDict) -> Dict[Text, Any]:
@@ -77,8 +112,9 @@ class ValidateBookingForm(FormValidationAction):
         
                 dispatcher.utter_message(text=f"OK! your reservation time will be set on the following date interval: \n from date: {formatted_from_date} at: {formatted_from_timing} \nto date: {formatted_to_date} at: {formatted_to_timing}")    
                     
-                return {"time":duckling_time}            
-        dispatcher.utter_message(text=f"please enter teh time correctly") 
-        return {"time":None}
-        
+                return {"time":duckling_time}   
+        else:         
+            dispatcher.utter_message(text=f"please enter the time correctly") 
+            return {"time":None}
+            
     
